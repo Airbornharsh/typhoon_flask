ee5fdf13e20011f70b5338cf0b18a7ad288e58c5
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-12 12:46:49 +0530

    .

--- model/student_thai.py
+++ model/student_thai.py
@@ -11,34 +11,35 @@ load_in_4bit = True
 
 def load_model():
     print("Loading model")
-    model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name="scb10x/typhoon-7b",
-        max_seq_length=max_seq_length,
-        dtype=dtype,
-        load_in_4bit=load_in_4bit,
-    )
+    return "", ""
+    # model, tokenizer = FastLanguageModel.from_pretrained(
+    #     model_name="scb10x/typhoon-7b",
+    #     max_seq_length=max_seq_length,
+    #     dtype=dtype,
+    #     load_in_4bit=load_in_4bit,
+    # )
 
-    model = FastLanguageModel.get_peft_model(
-        model,
-        r=16,
-        target_modules=[
-            "q_proj",
-            "k_proj",
-            "v_proj",
-            "o_proj",
-            "gate_proj",
-            "up_proj",
-            "down_proj",
-        ],
-        lora_alpha=16,
-        lora_dropout=0,
-        bias="none",
-        use_gradient_checkpointing=True,
-        random_state=3407,
-        use_rslora=False,
-        loftq_config=None,
-    )
-    return model, tokenizer
+    # model = FastLanguageModel.get_peft_model(
+    #     model,
+    #     r=16,
+    #     target_modules=[
+    #         "q_proj",
+    #         "k_proj",
+    #         "v_proj",
+    #         "o_proj",
+    #         "gate_proj",
+    #         "up_proj",
+    #         "down_proj",
+    #     ],
+    #     lora_alpha=16,
+    #     lora_dropout=0,
+    #     bias="none",
+    #     use_gradient_checkpointing=True,
+    #     random_state=3407,
+    #     use_rslora=False,
+    #     loftq_config=None,
+    # )
+    # return model, tokenizer
 
 
 # alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
