7d516896c070206e48a27e82365648bcb021336a
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-12 21:00:02 +0530

    added train

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from utils import fileOperations
 from model import student_thai
 
 typhoon_model, typhoon_tokenizer = None, None
+custom_model_and_tokenizer = {"thai_typhoon_model": {"data": (None, None), "index": 0}}
 
 app = Flask(__name__)
 
@@ -23,14 +24,17 @@ def upload_file(id):
     id_items = os.listdir(folder_path)
     new_id = len(id_items) + 1
 
+    folder_path += str(new_id) + "/"
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
     if "file" not in request.files:
         return "No file part"
     file = request.files["file"]
     if file.filename == "":
         return "No selected file"
-    # Here you can save the file to a desired location or process it
-    # For example, save it to a temporary directory
-    file.save("./sheets/" + id + "/" + str(new_id) + ".csv")
+    file.save("./sheets/" + id + "/" + str(new_id) + "/data.csv")
     return "File uploaded successfully"
 
 
@@ -46,11 +50,45 @@ def get_files(id):
 
 @app.route("/train/<string:id>", methods=["GET"])
 def model_train(id):
+    folder_path = "./sheets/" + id + "/"
+    if not os.path.exists(folder_path):
+        return "No files found"
+    id_items = os.listdir(folder_path)
+
     global typhoon_model, typhoon_tokenizer
     if typhoon_model is None or typhoon_tokenizer is None:
         typhoon_model, typhoon_tokenizer = student_thai.load_model()
-    print("Model loaded for ", id)
+
+    index = fileOperations.highest_numbered_file(id_items)
+
+    if index == custom_model_and_tokenizer[id]["index"]:
+        temp_model, temp_tokenizer = student_thai.load_data_and_train(
+            typhoon_model,
+            typhoon_tokenizer,
+            id,
+        )
+        custom_model_and_tokenizer[id] = {
+            "data": (typhoon_model, typhoon_tokenizer),
+            "index": index,
+        }
+    else:
+        temp_model, temp_tokenizer = student_thai.load_data_and_train(
+            custom_model_and_tokenizer[id]["data"][0],
+            custom_model_and_tokenizer[id]["data"][1],
+            id,
+        )
+        custom_model_and_tokenizer[id] = {
+            "data": (temp_model, temp_tokenizer),
+            "index": index,
+        }
+
     return json.dumps("Model training has started")
+
+
+@app.route("/predict/<string:id>", methods=["POST"])
+def model_predict(id):
+    print("Model loaded for ", id)
+    return json.dumps("Model prediction has started")
 
 
 if __name__ == "__main__":
--- utils/fileOperations.py
+++ utils/fileOperations.py
@@ -1,6 +1,5 @@
 import re
 
-
 def highest_numbered_file(file_names):
     # Extract numbers from file names
     numbers = [int(re.search(r"\d+", file_name).group()) for file_name in file_names]
@@ -9,6 +8,6 @@ def highest_numbered_file(file_names):
     highest_number = max(numbers)
 
     # Construct the highest file name
-    highest_file_name = f"{highest_number}.csv"
+    highest_file_name = f"{highest_number}"
 
     return highest_file_name
