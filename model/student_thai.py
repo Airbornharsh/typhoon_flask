48bbc255c930cfc16568d2d3038767b189b7bd38
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-04-13 19:45:32 +0530

    try and except

--- model/student_thai.py
+++ model/student_thai.py
@@ -53,78 +53,86 @@ def load_model():
 
 
 def load_data_and_train(model, tokenizer, id):
-    EOS_TOKEN = tokenizer.eos_token
+    try:
+        EOS_TOKEN = tokenizer.eos_token
 
-    def formatting_prompts_func(examples):
-        instructions = examples["instruction"]
-        inputs = examples["input"]
-        outputs = examples["output"]
-        texts = []
-        for instruction, input, output in zip(instructions, inputs, outputs):
-            text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
-            texts.append(text)
-        return {
-            "text": texts,
-        }
+        def formatting_prompts_func(examples):
+            instructions = examples["instruction"]
+            inputs = examples["input"]
+            outputs = examples["output"]
+            texts = []
+            for instruction, input, output in zip(instructions, inputs, outputs):
+                text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+                texts.append(text)
+            return {
+                "text": texts,
+            }
 
-    print("Loading data")
-    dataset = load_dataset("./sheets/" + id, split="train")
-    print(dataset)
-    dataset = dataset.map(
-        formatting_prompts_func,
-        batched=True,
-    )
-    print("Loading Map")
-    trainer = SFTTrainer(
-        model=model,
-        tokenizer=tokenizer,
-        train_dataset=dataset,
-        dataset_text_field="text",
-        max_seq_length=max_seq_length,
-        dataset_num_proc=2,
-        packing=False,
-        args=TrainingArguments(
-            per_device_train_batch_size=2,
-            gradient_accumulation_steps=4,
-            warmup_steps=5,
-            max_steps=60,
-            learning_rate=2e-4,
-            fp16=not torch.cuda.is_bf16_supported(),
-            bf16=torch.cuda.is_bf16_supported(),
-            logging_steps=1,
-            optim="adamw_8bit",
-            weight_decay=0.01,
-            lr_scheduler_type="linear",
-            seed=3407,
-            output_dir="outputs",
-        ),
-    )
-    print("Trainer")
+        print("Loading data")
+        dataset = load_dataset("./sheets/" + id, split="train")
+        print(dataset)
+        dataset = dataset.map(
+            formatting_prompts_func,
+            batched=True,
+        )
+        print("Loading Map")
+        trainer = SFTTrainer(
+            model=model,
+            tokenizer=tokenizer,
+            train_dataset=dataset,
+            dataset_text_field="text",
+            max_seq_length=max_seq_length,
+            dataset_num_proc=2,
+            packing=False,
+            args=TrainingArguments(
+                per_device_train_batch_size=2,
+                gradient_accumulation_steps=4,
+                warmup_steps=5,
+                max_steps=60,
+                learning_rate=2e-4,
+                fp16=not torch.cuda.is_bf16_supported(),
+                bf16=torch.cuda.is_bf16_supported(),
+                logging_steps=1,
+                optim="adamw_8bit",
+                weight_decay=0.01,
+                lr_scheduler_type="linear",
+                seed=3407,
+                output_dir="outputs",
+            ),
+        )
+        print("Trainer")
 
-    # @title Show current memory stats
-    gpu_stats = torch.cuda.get_device_properties(0)
-    start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
-    max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
-    print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
-    print(f"{start_gpu_memory} GB of memory reserved.")
+        # @title Show current memory stats
+        gpu_stats = torch.cuda.get_device_properties(0)
+        start_gpu_memory = round(
+            torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3
+        )
+        max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+        print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+        print(f"{start_gpu_memory} GB of memory reserved.")
 
-    trainer_stats = trainer.train()
+        trainer_stats = trainer.train()
 
-    # @title Show final memory and time stats
-    used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
-    used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
-    used_percentage = round(used_memory / max_memory * 100, 3)
-    lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
-    print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
-    print(
-        f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
-    )
-    print(f"Peak reserved memory = {used_memory} GB.")
-    print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
-    print(f"Peak reserved memory % of max memory = {used_percentage} %.")
-    print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+        # @title Show final memory and time stats
+        used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+        used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+        used_percentage = round(used_memory / max_memory * 100, 3)
+        lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+        print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+        print(
+            f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+        )
+        print(f"Peak reserved memory = {used_memory} GB.")
+        print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+        print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+        print(
+            f"Peak reserved memory for training % of max memory = {lora_percentage} %."
+        )
 
-    return model, tokenizer
+        return model, tokenizer
+    except Exception as e:
+        print("Error -> ", e)
+        return None, None
 
     # FastLanguageModel.for_inference(model)
     # inputs = tokenizer(
