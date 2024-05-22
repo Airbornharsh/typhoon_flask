e58e1691ab8bcd6feccf2c7067a7e3ccbb320961
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-22 16:23:32 +0530

    Revert "deleted all"
    This reverts commit 54e3078232d06cf70be81d3986be3362e1b62382.

---
+++ utils/fileOperations.py
@@ -0,0 +1,13 @@
+import re
+
+def highest_numbered_file(file_names):
+    # Extract numbers from file names
+    numbers = [int(re.search(r"\d+", file_name).group()) for file_name in file_names]
+
+    # Find the highest number
+    highest_number = max(numbers)
+
+    # Construct the highest file name
+    highest_file_name = f"{highest_number}"
+
+    return highest_file_name
